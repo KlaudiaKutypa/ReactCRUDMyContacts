5c7da4ae5a09a873c9da8caac3ff14f06657e6c3
KlaudiaKutypa <klaudia.kutypa@gmail.com>
2023-02-19 14:05:13 +0100

    mockAPI Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../scss/Home.scss';
+import { useState, useEffect } from 'react';
+
+const API = "https://63ef9746c59531ccf172eaee.mockapi.io/my_contacts"
 
 
 function Home() {
+
+    const [contact, setContact] = useState([])
+
+    const handleGet = () => {
+        fetch(`${API}`)
+        .then((response) => response.json())
+        .then((data) => setContact(data))
+    }
+
+    useEffect (() => {
+        handleGet();
+    },[])
+
+
   return (
     <div className="home-container">
       <div className="header">
@@ -21,16 +38,18 @@ function Home() {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Klaudia</td>
-                <td>7626228</td>
-                <td>klaudia@gmail.com</td>
+            {contact.map((item) => (
+            <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>+{Math.random().toFixed(2)*100}  {item.mobile}{Math.ceil(Math.random()* 10000000)}</td>
+                <td>{item.email}</td>
                 <td>
                     <button>Details</button>
                     <button>Edit</button>
                     <button>Delete</button>
                 </td>
             </tr>
+            ))}
         </tbody>
       </table>
     </div>
